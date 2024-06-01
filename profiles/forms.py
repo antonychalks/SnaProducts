6d444f00582634922d41ec8ecad29ee66dd7535f
antonychalks <antonychalkley@hotmail.co.uk>
2024-06-01 18:24:55 +0100

    Added form

---
+++ profiles/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_first_line_address': 'First Line of Address',
+            'default_second_line_address': 'Second Line of Address',
+            'default_county': 'County',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 def profile(request):
     """ A view that renders the profile page """
-    context = {
+    profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
 
+    context = {
+        'form': form
     }
     return render(request, 'profiles/profile.html')
