c0163dd1e73a15701234384a3ee5955b97d292c1
antonychalks <antonychalkley@hotmail.co.uk>
2024-06-05 17:52:43 +0100

    Fixed a typo in the checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -115,8 +115,8 @@ def checkout(request):
                 'country': profile.default_country,
                 'postcode': profile.default_postcode,
                 'town_or_city': profile.default_town_or_city,
-                'street_address1': profile.default_street_address1,
-                'street_address2': profile.default_street_address2,
+                'first_line_address': profile.first_line_address,
+                'second_line_address': profile.second_line_address,
                 'county': profile.default_county,
             })
         except UserProfile.DoesNotExist:
