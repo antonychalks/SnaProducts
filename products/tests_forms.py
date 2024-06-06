3d74051ceee8e6129de70b4eb53e68595e50ac05
antonychalks <antonychalkley@hotmail.co.uk>
2024-06-06 07:11:29 +0100

    Added product forms test

--- products/tests_forms.py
+++ products/tests_forms.py
@@ -9,35 +9,37 @@ class TestProductManagementForm(TestCase):
     def setUp(self):
         # Set up some test data
         self.misc_category = Category.objects.create(name="test", type=0, display_name="Test")
-        self.regular_category = Category.objects.create(name="regular", type=0)
-        self.children_category = Category.objects.create(name="child", type=1, parent=self.regular_category)
+        self.regular_category = Category.objects.create(name="regular", type=0, display_name="Regular")
+        self.children_category = Category.objects.create(name="child", type=1,
+                                                         parent=self.regular_category,
+                                                         display_name="child_test")
+
+        self.misc_category.save()
+        self.regular_category.save()
+        self.children_category.save()
 
     def test_form_initialization(self):
         form = ProductManagementForm()
         expected_choices = [
-            (self.misc_category.id, self.misc_category.get_display_name() + " (Default)"),
-            (self.children_category.id, self.children_category.get_display_name())
+            ('Test', []),
+            ('Regular', [(self.children_category.id, self.children_category.get_display_name())])
         ]
-        self.assertEqual(form.fields['category'].choices, expected_choices)
+        actual_choices = form.fields['category'].choices
+        self.assertEqual(expected_choices, actual_choices)
         for field_name, field in form.fields.items():
             assert 'border-black rounded-0' in field.widget.attrs['class']
 
-    def test_required_fields(self):
-        form = ProductManagementForm()
-        self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors, {
-            'name': ['This field is required.'],
-            'description': ['This field is required.'],
-            'price': ['This field is required.'],
-        })
-
-
     def test_product_management_form_valid_with_all_required_fields(self):
         form = ProductManagementForm({
-            'category': self.misc_category.id,
+            'category': self.children_category,
             'name': 'Test_product',
             'description': 'Test_product Description',
             'price': 99.99,
-
+            # Add other fields if they are required
         })
+
+        # print out the form errors here
+        if not form.is_valid():
+            print(form.errors)
+
         self.assertTrue(form.is_valid())
